8c2eaf9e36d5ce448c7c1e5fc1f9a4a1069a4fdf
Hrishikeshkurhe <hrishikeshkurhe03@gmail.com>
2025-11-19 18:39:38 +0530

    team updated

--- Frontend/src/common/pages/Team.jsx
+++ Frontend/src/common/pages/Team.jsx
@@ -74,7 +74,7 @@ const Team = () => {
 
       {/* ⭐ MENTORS */}
       <section className="max-w-7xl mx-auto px-6 mb-20">
-        <h3 className="text-3xl font-bold text-gray-800 ml-130 mb-8">Our Mentors</h3>
+        <h3 className="text-3xl font-bold text-center text-gray-800 mb-8">Our Mentors</h3>
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
           {mentors.map((m, i) => (
